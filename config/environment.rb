abd178956345e268200699be516aeb4f7b9cff4a
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-15 12:35:28 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ configure :development do
   set :database, 'sqlite3:db/database.db'
 end 
 
-
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
